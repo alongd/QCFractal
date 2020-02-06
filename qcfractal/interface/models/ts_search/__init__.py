2cb93c0e07624677302a0060f63d5d8a3465bce3
Alon Grinberg Dana <alongd@mit.edu>
2020-02-05 20:40:10 -0500

    Added the rmg_db module

--- qcfractal/interface/models/ts_search/__init__.py
+++ qcfractal/interface/models/ts_search/__init__.py
@@ -4,3 +4,8 @@ initialize the ts_guess module
 
 from .ts_search import TSSearch
 from .heuristics import generate_ts_guesses_by_heuristics
+from .rmg_db import (make_rmg_database_object,
+                     load_families_only,
+                     load_rmg_database,
+                     determine_reaction_family,
+                     determine_rmg_kinetics)
